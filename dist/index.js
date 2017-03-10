deaf947a82716ff9c5563b6894b667d85b208e60
Marco Scannadinari <m@scannadinari.co.uk>
2017-03-10 11:10:39 +0000

    Build 4.1.0

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,15 @@
 	(global.quicktap = factory());
 }(this, (function () { 'use strict';
 
-const version = "3.3.1";
+const version = "4.1.0";
 
-// @preserve - quicktap by Marco Scannadinari, MIT licensed
+// @preserve - quicktap by Marco Scannadinari <m@scannadinari.co.uk>, MIT licensed
 
-const [versionMajor, versionMinor, versionPatch] = version.split(`.`);
+const defaultActiveClass = 'active';
+const [versionMajor, versionMinor, versionPatch] = version.split('.');
 
-if (typeof window === `undefined`) {
-	throw new Error(`quicktap can only be used in the browser`);
+if (typeof window === 'undefined') {
+	throw new Error('quicktap can only be used in the browser');
 }
 
 function makeActivateDeactivateFns(className) {
@@ -20,7 +21,7 @@ function makeActivateDeactivateFns(className) {
 	return {
 		activate(evt) {
 			const el = evt.currentTarget;
-			const activateEvt = new CustomEvent(`activate`);
+			const activateEvt = new CustomEvent('activate');
 
 			el.dispatchEvent(activateEvt);
 			el.classList.add(className);
@@ -28,7 +29,7 @@ function makeActivateDeactivateFns(className) {
 
 		deactivate(evt) {
 			const el = evt.currentTarget;
-			const deactivateEvt = new CustomEvent(`deactivate`);
+			const deactivateEvt = new CustomEvent('deactivate');
 
 			el.dispatchEvent(deactivateEvt);
 			el.classList.remove(className);
@@ -37,33 +38,33 @@ function makeActivateDeactivateFns(className) {
 }
 
 // detect touch events support
-const touchEnabled = `ontouchstart` in window;
+const touchEnabled = 'ontouchstart' in window;
 
 // detect passive event listener support
 
 let supportsPassive = false;
 
 try {
-	const opts = Object.defineProperty({}, `passive`, {
+	const opts = Object.defineProperty({}, 'passive', {
 		get() { supportsPassive = true; }
 	});
 
-	window.addEventListener(`x`, null, opts);
+	window.addEventListener('x', null, opts);
 } catch (err) {}
 
 function quicktap(elOrEls, options={}) {
 	let els = [];
 
-	if (typeof options !== `object`) {
+	if (typeof options !== 'object') {
 		throw new Error(`options must be an object (got '${options}'`);
 	}
 
 	// class is not a reserved property name but is a reserved identifier
 	let className = options.class;
 
-	if (typeof className === `undefined`) {
-		className = quicktap.class;
-	} else if (typeof className !== `string`) {
+	if (typeof className === 'undefined') {
+		className = defaultActiveClass;
+	} else if (typeof className !== 'string') {
 		throw new Error(`options.class must be string (got '${elOrEls}')`);
 	}
 
@@ -71,7 +72,7 @@ function quicktap(elOrEls, options={}) {
 
 	let context = options.context;
 
-	if (typeof context === `undefined`) {
+	if (typeof context === 'undefined') {
 		context = document;
 	} else if (
 		!(context instanceof Document)
@@ -85,7 +86,7 @@ function quicktap(elOrEls, options={}) {
 
 	if (elOrEls instanceof HTMLElement) {
 		els.push(elOrEls);
-	} else if (typeof elOrEls === `string`) {
+	} else if (typeof elOrEls === 'string') {
 		const matchingEls = context.querySelectorAll(elOrEls);
 		if (matchingEls !== null) {
 			// NodeList -> Array
@@ -107,24 +108,24 @@ function quicktap(elOrEls, options={}) {
 		if (supportsPassive) {
 			const opts = {passive: true};
 
-			el.addEventListener(`mousedown`, activate, opts);
-			el.addEventListener(`mouseup`, deactivate, opts);
-			el.addEventListener(`mouseleave`, deactivate, opts);
+			el.addEventListener('mousedown', activate, opts);
+			el.addEventListener('mouseup', deactivate, opts);
+			el.addEventListener('mouseleave', deactivate, opts);
 
 			if (touchEnabled) {
-				el.addEventListener(`touchstart`, activate, opts);
-				el.addEventListener(`touchcancel`, deactivate, opts);
-				el.addEventListener(`touchend`, deactivate,  opts);
+				el.addEventListener('touchstart', activate, opts);
+				el.addEventListener('touchcancel', deactivate, opts);
+				el.addEventListener('touchend', deactivate,  opts);
 			}
 		} else {
-			el.addEventListener(`mousedown`, activate, false);
-			el.addEventListener(`mouseup`, deactivate, false);
-			el.addEventListener(`mouseleave`, deactivate, false);
+			el.addEventListener('mousedown', activate, false);
+			el.addEventListener('mouseup', deactivate, false);
+			el.addEventListener('mouseleave', deactivate, false);
 
 			if (touchEnabled) {
-				el.addEventListener(`touchstart`, activate, false);
-				el.addEventListener(`touchcancel`, deactivate, false);
-				el.addEventListener(`touchend`, deactivate, false);
+				el.addEventListener('touchstart', activate, false);
+				el.addEventListener('touchcancel', deactivate, false);
+				el.addEventListener('touchend', deactivate, false);
 			}
 		}
 	}
@@ -132,7 +133,6 @@ function quicktap(elOrEls, options={}) {
 	return els;
 }
 
-quicktap.class = `active`;
 quicktap.version = {
 	major: Number.parseInt(versionMajor, 10),
 	minor: Number.parseInt(versionMinor, 10),
